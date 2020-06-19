be35926830df819ff2940844706a3a4ead7fdc61
berilbayram <berilbayram@gmail.com>
2020-06-19 20:50:43 +0300

    Android fundamentals 04.3: Menus and pickers are done and Homework is included.

--- app/src/main/java/com/example/droiddeserts_vol2/OrderActivity.java
+++ app/src/main/java/com/example/droiddeserts_vol2/OrderActivity.java
@@ -1,11 +1,11 @@
 package com.example.droiddeserts_vol2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
@@ -71,5 +71,20 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string + "/" + year_string);
+        Toast.makeText(this, "Date: " + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 }
